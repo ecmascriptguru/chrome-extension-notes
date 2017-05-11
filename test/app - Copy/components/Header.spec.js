d181ce8ea5564707c880009340379524f93d6ef1
ecmascriptguru <ecmascript.guru@gmail.com>
2017-05-11 13:46:55 -0400

    test backup created

---
+++ test/app - Copy/components/Header.spec.js
@@ -0,0 +1,45 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import React from 'react';
+import TestUtils from 'react-dom/test-utils';
+import ReactShallowRenderer from 'react-test-renderer/shallow';
+import Header from '../../../app/components/Header';
+import TodoTextInput from '../../../app/components/TodoTextInput';
+
+function setup() {
+  const props = {
+    addTodo: sinon.spy()
+  };
+
+  const renderer = new ReactShallowRenderer();
+  renderer.render(<Header {...props} />);
+  const output = renderer.getRenderOutput();
+
+  return { props, output, renderer };
+}
+
+describe('todoapp Header component', () => {
+  it('should render correctly', () => {
+    const { output } = setup();
+
+    expect(output.type).to.equal('header');
+
+    const [h1, input] = output.props.children;
+
+    expect(h1.type).to.equal('h1');
+    expect(h1.props.children).to.equal('Personal Notes');
+
+    expect(input.type).to.equal(TodoTextInput);
+    expect(input.props.newTodo).to.equal(true);
+    expect(input.props.placeholder).to.equal('What needs to be done?');
+  });
+
+  it('should call addTodo if length of text is greater than 0', () => {
+    const { output, props } = setup();
+    const input = output.props.children[1];
+    input.props.onSave('');
+    expect(props.addTodo.callCount).to.equal(0);
+    input.props.onSave('Use Redux');
+    expect(props.addTodo.callCount).to.equal(1);
+  });
+});
